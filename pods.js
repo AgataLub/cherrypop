808b455ce1f3dced973d2dbed35254ef5a3531ba
AgataLub <70294512+AgataLub@users.noreply.github.com>
2020-12-18 17:16:22 +0100

    3

--- pods.js
+++ pods.js
@@ -6,7 +6,7 @@ window.addEventListener("load", fetchQuestions);
 
     function fetchQuestions() {
         console.log("fetchQuestion");
-        fetch("https://agataswistak.com/wordpress/wp-json/wp/v2/ap-quiz")
+        fetch("https://agataswistak.com/wordpress/wp-json/wp/v2/faq")
             .then(function (response) {
                 console.log(response)
                 return response.json();
@@ -30,37 +30,12 @@ window.addEventListener("load", fetchQuestions);
         var temp = document.querySelector("#templateID");
         var myCopy = temp.cloneNode(true);
 
-        myCopy.querySelector(".number_span").textContent = myQuestion.number;
-        myCopy.querySelector("h1").textContent = myQuestion.quiz_title;
-        myCopy.querySelector(".blurb").textContent = myQuestion.blurb;
-        myCopy.querySelector("#specificQuestion").textContent = myQuestion.question;
+        myCopy.querySelector(".faq-question").textContent = myQuestion.question;
+        myCopy.querySelector(".faq-answer").textContent = myQuestion.answer;
 
-        if (myQuestion.number < 4) {
-            myCopy.querySelector(".table").classList.remove("display_none");
-            myCopy.querySelector("#answer1").textContent = myQuestion.answers;
-            myCopy.querySelector("#answer2").textContent = myQuestion.answers2;
-            myCopy.querySelector("#answer3").textContent = myQuestion.answers3;
-            myCopy.querySelector("#answer4").textContent = myQuestion.answers4;
-            myCopy.querySelector("#answer5").textContent = myQuestion.answers5;
-
-        } else {
-            myCopy.querySelector(".slidecontainer").classList.remove("display_none");
-
-        }
-
-        var myQuestionImage = myQuestion.image_quiz.guid;
-        console.log(myQuestionImage);
-
-        myCopy.querySelector(".illustration").setAttribute("src", myQuestionImage);
-
-        var test = myQuestion.number;
-        console.log(test);
-        console.log('#template_place' + myQuestion.number);
-
-        const parentElem = document.querySelector("#template_place" + myQuestion.number);
+        const parentElem = document.querySelector(".faq-container");
         parentElem.appendChild(myCopy);
     }
-}
 
 //<<<<<<< HEAD
 //var slider = document.querySelector("#myRange");
